3530ef27e8bf8d90f63b3f1c7203e5cc6963e86f
Anastasiia <anstasiakim2789@gmail.com>
2023-06-22 14:27:23 +0200

    added onBlur errors

--- Components/LoginScreenComponent.tsx
+++ Components/LoginScreenComponent.tsx
@@ -1,24 +1,25 @@
 /* eslint-disable prettier/prettier */
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React, { useState } from 'react';
-import {View, Text, StyleSheet, TextInput, Pressable, Button, Alert } from 'react-native';
+import React, { useState,useEffect  } from 'react';
+import {View, Text, StyleSheet, TextInput, Pressable, Button, Alert,TextInputFocusEventData, NativeSyntheticEvent } from 'react-native';
 import { RootStackParamList } from './AppNavigator';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type InputFunction = (param: string ) => void;
-
+type onBlurFunction = (event: NativeSyntheticEvent<TextInputFocusEventData>) => void;
 type LoginScreenComponentProps=NativeStackScreenProps<RootStackParamList, 'Login'>
 
 interface BoxProps {
   placeholder:string,
   value:string,
   secureTextEntry:boolean,
-  callBack:InputFunction
+  callBack:InputFunction,
+  onBlur?: onBlurFunction;
 }
 
-const Box: React.FC<BoxProps>=({placeholder,value,secureTextEntry, callBack})=>(
+const Box: React.FC<BoxProps>=({placeholder,value,secureTextEntry, callBack, onBlur})=>(
     <View style={{justifyContent:"center",alignItems:"center" , width:"100%"}}>
-        <TextInput style={{margin: 8, borderWidth:1, width:"90%", borderRadius:70, padding:10}} placeholder={placeholder} value={value} secureTextEntry={secureTextEntry} onChangeText={(text)=>{callBack(text)}}/>
+        <TextInput style={{margin: 8, borderWidth:1, width:"90%", borderRadius:70, padding:10}} placeholder={placeholder} value={value} secureTextEntry={secureTextEntry} onChangeText={(text)=>{callBack(text)}} onBlur={onBlur}/>
     </View>
 );
 
@@ -26,8 +27,38 @@ const LoginScreenComponent:React.FC<LoginScreenComponentProps>=(props)=>{
 
     const [email, setEmail]=useState<string>("")
     const [password, setPassword]=useState<string>("")
+    const [errorEmail, setErrorEmail] = useState<string>("");
+    const [errorPassword, setErrorPassword] = useState<string>("");
 
-    let login=async()=>{
+    useEffect(()=>{
+      setErrorEmail("");
+    },[email]);
+
+    useEffect(()=>{
+      setErrorPassword("");
+    },[password]);
+
+    const onBlurEmail=()=>{
+      let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (email != null && email != "" && !emailPattern.test(email)){
+        setErrorEmail("Wrong email format");
+      } else if(email==""){
+        setErrorEmail("Enter your email");
+      } else {
+        setErrorEmail("");
+      }
+    };
+
+    const onBlurPasswor=()=>{
+      if (password==""){
+        setErrorPassword("Enter your password");
+      } else {
+        setErrorPassword("");
+      }
+    };
+
+    let login = async()=>{
+      if (errorEmail=="" && errorPassword!==""){
         let response = await fetch ("http://192.168.31.75:4000/public/user/verification",{ 
           method: 'POST',
           headers: {
@@ -54,15 +85,18 @@ const LoginScreenComponent:React.FC<LoginScreenComponentProps>=(props)=>{
             AsyncStorage.setItem('userId', data.userId);
           }
         }
+      }
 
     };
     return (
       <View style={{ height:"100%"}}>
         <View style={[styles.layout, {height:"80%"}]}>
           <Text style={styles.title}>Insta dog</Text>
-          <Box value={email} placeholder="Email" callBack={setEmail} secureTextEntry={false}/>
-          <Box value={password} placeholder="Password" callBack={setPassword} secureTextEntry={true}  />
-          <Pressable  style={styles.button} onPress={login}>
+          <Box value={email} placeholder="Email" callBack={setEmail} secureTextEntry={false} onBlur={onBlurEmail}/>
+          {errorEmail !="" && <Text style={styles.alert}>{errorEmail}</Text>}
+          <Box value={password} placeholder="Password" callBack={setPassword} secureTextEntry={true} onBlur={onBlurPasswor}/>
+          {errorPassword !="" && <Text style={styles.alert}>{errorPassword}</Text>}
+          <Pressable style={styles.button} onPress={login}>
               <Text style={styles.textInButton}>Sign in</Text>
           </Pressable>
         </View>
@@ -102,5 +136,11 @@ const LoginScreenComponent:React.FC<LoginScreenComponentProps>=(props)=>{
       letterSpacing: 0.25,
       color: 'white',
     },
+    alert:{
+      color:"red",
+      width:"90%"
+    }
   });
 export default LoginScreenComponent;
+
+
